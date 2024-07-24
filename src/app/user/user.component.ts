b18a312b93b332a761a78e4cfa30d69c8dc7828d
devmanoshri <manoshri.no@gmail.com>
2024-07-24 14:01:27 +0200

    user component add

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+ console.log(randomIndex);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+   selectedUser = DUMMY_USERS[randomIndex];
+
+
+}
